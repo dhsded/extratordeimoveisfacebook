d94131df441f17b7d273aa890a6b97ad77ea7198
Diego Dutra <dhsded@github.com>
2026-06-16 10:31:33 -0300

    fix: import useLocation in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { WSProvider } from './context/WSContext';
 import { ThemeProvider } from './context/ThemeContext';
 import Sidebar from './components/Sidebar';
